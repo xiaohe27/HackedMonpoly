ff00e315a4787bd4142ebf2608e346bc4dc056d3
HE XIAO <hexiao2@illinois.edu>
2015-01-02 18:31:16 -0600

    modify monpoly for counting events

---
+++ src/make.sh
@@ -0,0 +1,52 @@
+ocamlc -c  dllist.mli
+ocamlc -c  dllist.ml
+
+ocamlc -c  mqueue.mli
+ocamlc -c  mqueue.ml
+
+ocamlc -c  misc.mli
+ocamlc -c  misc.ml
+
+ocamlc -c  predicate.mli
+ocamlc -c  predicate.ml
+
+ocamlc -c  MFOTL.mli
+ocamlc -c  MFOTL.ml
+
+ocamlc -c  perf.mli
+ocamlc -c  perf.ml
+
+ocamlc -c  tuple.mli
+ocamlc -c  tuple.ml
+
+ocamlc -c  relation.mli
+ocamlc -c  relation.ml
+
+ocamlc -c  table.mli
+ocamlc -c  table.ml
+
+ocamlc -c  db.mli
+ocamlc -c  db.ml
+
+ocamlc -c filter_rel.mli
+ocamlc -c filter_rel.ml
+
+ocamlyacc  log_parser.mly
+ocamlc -c  log_parser.mli
+ocamlc -c  log_parser.ml
+
+ocamllex  log_lexer.mll
+ocamlc -c  log_lexer.ml
+
+ocamlc -c filter_empty_tp.mli
+ocamlc -c filter_empty_tp.ml
+
+ocamlc -c  log.mli
+ocamlc -c  log.ml
+
+ocamlc -c  sliding.mli
+ocamlc -c  sliding.ml 
+
+ocamlc -c algorithm.ml
+
+
